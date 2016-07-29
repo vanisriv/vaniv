56276199a85edaa29287053013b3658ef6d7fbab
vanisriv <vanisriv.cs13@bitsathy.ac.in>
2016-07-29 14:34:38 +0530

    Rename posneg.c to posnega.c

---
+++ posnega.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+ int a;
+ scanf("%d",&a);
+  if(a==0)
+    {
+      printf("Zero");
+     }
+  else if(a>0)
+    {
+        printf("Positive");
+    }
+  else
+    printf("Negative");
+	return 0;
+}
+
